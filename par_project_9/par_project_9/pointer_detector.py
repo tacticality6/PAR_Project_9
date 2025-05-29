881d0e82b23bb5fa94c9e956a055c0068716486c
maraperera <reach.anthony.perera.1@gmail.com>
2025-05-29 11:26:10 +1000

    Initialize ViSP blob tracking

--- par_project_9/par_project_9/pointer_detector.py
+++ par_project_9/par_project_9/pointer_detector.py
@@ -2,6 +2,9 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, qos_profile_sensor_data
+import numpy as np
+import visp_ros
+import visp
 
 class PointerDetector(Node):
     def __init__(self):
@@ -12,6 +15,20 @@ class PointerDetector(Node):
         self.declare_parameter("pointer_hsv_upper", [15, 255, 255])
         self.declare_parameter("touch_threshold", 0.15)  # meters
         self.declare_parameter("blob_size_threshold", 500)  # pixels
+
+        # ---------- ViSP Initialization ----------
+        self.tracker = visp_ros.BlobTracker()
+        self.cam = visp.CameraParameters()
+        # Dummy initialization, will be updated by camera_info_callback
+        self.cam.initPersProjWithoutDistortion(600, 600, 320, 240)
+        self.tracker.setCameraParameters(self.cam)
+
+        # Set orange color parameters
+        lower = np.array(self.get_parameter("pointer_hsv_lower").value)
+        upper = np.array(self.get_parameter("pointer_hsv_upper").value)
+        self.tracker.setHSVColorRange(
+            visp.ColorHSV(lower[0], upper[0], lower[1], upper[1], lower[2], upper[2])
+        )
 
         self.get_logger().info("🚦 Pointer Detector node starting up...")
 
