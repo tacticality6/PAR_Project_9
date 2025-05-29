3fe81a42571cd6d18e4fa8d57d0d0b653b8d99e4
maraperera <reach.anthony.perera.1@gmail.com>
2025-05-29 11:24:46 +1000

    Initializing configurable ROS parameters

--- par_project_9/par_project_9/pointer_detector.py
+++ par_project_9/par_project_9/pointer_detector.py
@@ -6,6 +6,13 @@ from rclpy.qos import QoSProfile, qos_profile_sensor_data
 class PointerDetector(Node):
     def __init__(self):
         super().__init__("pointer_detector")
+
+        # ---------- Parameters ----------
+        self.declare_parameter("pointer_hsv_lower", [5, 100, 100])
+        self.declare_parameter("pointer_hsv_upper", [15, 255, 255])
+        self.declare_parameter("touch_threshold", 0.15)  # meters
+        self.declare_parameter("blob_size_threshold", 500)  # pixels
+
         self.get_logger().info("🚦 Pointer Detector node starting up...")
 
 def main():
