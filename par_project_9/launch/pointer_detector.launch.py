6b71b1519bd453bdf1d19b44f1bb0d89e844c8d4
maraperera <reach.anthony.perera.1@gmail.com>
2025-06-11 12:21:46 +1000

    Quick executable typo fix + add

--- par_project_9/launch/pointer_detector.launch.py
+++ par_project_9/launch/pointer_detector.launch.py
@@ -5,17 +5,17 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='par_project_9',
-            executable='aruco_detector.py',
+            executable='aruco_detector',
             name='aruco_detector'
         ),
         Node(
             package='par_project_9',
-            executable='pointer_detector.py',
+            executable='pointer_detector',
             name='pointer_detector'
         ),
         Node(
             package='par_project_9',
-            executable='visual_servoing_node.py',
+            executable='visual_servoing_node',
             name='visual_servoing_node'
         )
     ])
