d6e5449e53a1a56da3d1a57b91b53e1d7212ee81
maraperera <reach.anthony.perera.1@gmail.com>
2025-06-16 10:04:47 +1000

    Add static transform publisher for OAK-D camera

--- par_project_9/launch/pointer_detector.launch.py
+++ par_project_9/launch/pointer_detector.launch.py
@@ -2,7 +2,29 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    # X: forward, Y: left, Z: up
+    cam_x = 0.05   # 5cm forward from base_link center
+    cam_y = 0.0    # centered horizontally
+    cam_z = 0.15   # 15cm above base_link center
+
     return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_cam_tf_pub',
+            arguments=[
+                '--x', str(cam_x),
+                '--y', str(cam_y),
+                '--z', str(cam_z),
+                '--yaw', '0',
+                '--pitch', '0',
+                '--roll', '0',
+                '--frame-id', 'base_link',
+                '--child-frame-id', 'oak_rgb_camera_optical_frame'
+            ],
+        ),
+
         Node(
             package='par_project_9',
             executable='aruco_detector',
